0e3102e2fc1443e06e25c8003bfa01a817d7895f
Najoa Prome <najoa2252@gmail.com>
2025-04-20 20:01:44 +0600

    Your update message

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-const colorMap = {
-    red: 'red',
-    green: 'green',
-    blue: 'blue',
-    pink: 'pink',
-    black: 'black'
-};
-
-const buttons = document.querySelectorAll('li');
-
-buttons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        let colorClass = this.classList.value;
-        let selectedColor = colorMap[colorClass] || 'black';
-        document.getElementById('body').style.backgroundColor = selectedColor;
-    });
-});
+const divElement = document.querySelectorAll('#myDiv');
+console.log(divElement)
